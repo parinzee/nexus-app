57aa6e15bd01a49be5d1765d19d82c0557c3a7d0
Parinz <parinzee@protonmail.com>
2021-06-05 03:57:42 +0000

    Finished SQL Wrapper

--- Backend/sqlwrapper.py
+++ Backend/sqlwrapper.py
@@ -1,36 +1,81 @@
 import sqlite3
 
-def insertEvent(eventDate, eventDesc):
+def insertItem(eventTitle, eventDesc, itemType):
     db = sqlite3.connect('main.db')
     cur = db.cursor()
 
-    cur.execute('''CREATE TABLE IF NOT EXISTS events(
-        id integer PRIMARY KEY,
-        eventDate text NOT NULL,
-        eventDesc text NOT NULL);
-    ''')
+    if itemType == "events":
+        cur.execute('''CREATE TABLE IF NOT EXISTS events(
+            id integer PRIMARY KEY,
+            eventTitle text NOT NULL,
+            eventDesc text NOT NULL);
+        ''')
 
-    cur.execute('''
-        INSERT INTO events(eventDate, eventDesc) VALUES(?, ?);
-        ''', (eventDate, eventDesc))
+        cur.execute('''
+            INSERT INTO events(eventTitle, eventDesc) VALUES(?, ?);
+            ''', (eventTitle, eventDesc))
+
+    elif itemType == "activities":
+        cur.execute('''CREATE TABLE IF NOT EXISTS activities(
+            id integer PRIMARY KEY,
+            eventTitle text NOT NULL,
+            eventDesc text NOT NULL);
+        ''')
+
+        cur.execute('''
+            INSERT INTO activities(eventTitle, eventDesc) VALUES(?, ?);
+            ''', (eventTitle, eventDesc))
+    
+    elif itemType == "competitions":
+        cur.execute('''CREATE TABLE IF NOT EXISTS competitions(
+            id integer PRIMARY KEY,
+            eventTitle text NOT NULL,
+            eventDesc text NOT NULL);
+        ''')
+
+        cur.execute('''
+            INSERT INTO competitions(eventTitle, eventDesc) VALUES(?, ?);
+            ''', (eventTitle, eventDesc))
+    else:
+        pass
+
 
     db.commit()
     db.close()
 
-def listItems():
+def listItems(itemType):
     db = sqlite3.connect('main.db')
     cur = db.cursor()
 
-    cur.execute('''CREATE TABLE IF NOT EXISTS events(
-        id integer PRIMARY KEY,
-        eventDate text NOT NULL,
-        eventDesc text NOT NULL);
-    ''')
+    if itemType == "events":
+        cur.execute('''CREATE TABLE IF NOT EXISTS events(
+            id integer PRIMARY KEY,
+            eventTitle text NOT NULL,
+            eventDesc text NOT NULL);
+        ''')
 
-    cur.execute("SELECT * FROM events")
+        cur.execute("SELECT * FROM events")
+
+    elif itemType == "activities":
+        cur.execute('''CREATE TABLE IF NOT EXISTS activities(
+            id integer PRIMARY KEY,
+            eventTitle text NOT NULL,
+            eventDesc text NOT NULL);
+        ''')
+
+        cur.execute("SELECT * FROM activities")
+
+    elif itemType == "competitions":
+        cur.execute('''CREATE TABLE IF NOT EXISTS competitions(
+            id integer PRIMARY KEY,
+            eventTitle text NOT NULL,
+            eventDesc text NOT NULL);
+        ''')
+
+        cur.execute("SELECT * FROM competitions")
+    
+    else:
+        pass
+
     events = cur.fetchall()
-
     return events
-
-insertEvent("28/56/2021", "A test event")
-print(listItems())
