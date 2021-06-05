c3e5c8af130e0cfc38db4f08c5da3632b0f5664e
Parinz <parinzee@protonmail.com>
2021-06-05 04:26:24 +0000

    Type checking and whatnot

--- Backend/main.py
+++ Backend/main.py
@@ -1,13 +1,35 @@
 import uvicorn
 from fastapi import FastAPI
 from os import environ
+from sqlwrapper import insertItem, listItems
+from pydantic import BaseModel, ValidationError, validator
 
 app = FastAPI()
-passkey = environ.get('PASSKEY')
+
+class Item(BaseModel):
+    eventTitle: str
+    eventDesc: str
+    itemType: str
+
+    @validator("itemType")
+    def validType(cls, v):
+        if v != "activities" and v != "events" and v != "competitions":
+            raise ValidationError("It must be activities, events, or competitions")
+        return v
 
 @app.get("/")
-def listItems():
-    return "Hello World"
+async def listItem():
+    items = dict()
+    for i in ["activities", "competitions", "events"]:
+        items[i] = listItems(i)
+    
+    return items
+
+@app.post("/insertItem/")
+async def insert(item: Item):
+    return item
+    
+    
 
 
 if __name__ == "__main__":
