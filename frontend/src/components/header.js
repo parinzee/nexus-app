b35d6e80b66f3d3b5385ebbcca1be10d5f99901a
Parinz <parinzee@protonmail.com>
2021-06-10 05:43:03 +0000

    React shitshut

---
+++ frontend/src/components/header.js
@@ -0,0 +1,13 @@
+const header = () => {
+    return (
+        <div className="header">
+            <div id="header-logo"></div>
+            <div id="header-text">
+                <div id="text-upper">Elevate BCIS Together</div>
+                <div id="text-lower">EXUS</div>
+            </div>
+        </div>
+    );
+};
+
+export default header;
