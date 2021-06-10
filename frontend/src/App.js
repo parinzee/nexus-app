82ce3ee7e48e61cf06f3edb1bdfb2205af30fa2f
Parinz <parinzee@protonmail.com>
2021-06-10 08:39:01 +0000

    Finally centered div

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
+import Header from "./components/Header";
 function App() {
     return (
         <div className="App">
-            <h1>Nexus</h1>
+            <Header />
         </div>
     );
 }
