10cea150a52cd51e10217b22e7dbf5896696869f
Parinz <parinzee@protonmail.com>
2021-06-12 08:13:28 +0000

    Second Element

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -2,11 +2,17 @@ import MenuSelector from "./MenuSelector";
 
 // * Photo Assets
 import sleepingMan from "../assets/sleepingMan.gif";
+import typingMan from "../assets/typingMan.gif";
 
 const Menu = () => {
     return (
         <div className="menu">
             <MenuSelector id="links" image={sleepingMan} text="LINKS" />
+            <MenuSelector
+                id="events"
+                image={typingMan}
+                text="UPCOMING EVENTS"
+            />
         </div>
     );
 };
