09e66d28e15f638488cd281e830d4b4b227c712e
Parinz <parinzee@protonmail.com>
2021-06-13 09:46:52 +0000

    Trying to add 3rd layout

--- frontend/src/components/Menu.js
+++ frontend/src/components/Menu.js
@@ -3,6 +3,7 @@ import MenuSelector from "./MenuSelector";
 // * Photo Assets
 import sleepingMan from "../assets/sleepingMan.gif";
 import typingMan from "../assets/typingMan.gif";
+import AGuy from "../assets/A+Guy.gif";
 
 const Menu = () => {
     return (
@@ -12,6 +13,11 @@ const Menu = () => {
                 id="events"
                 image={typingMan}
                 text="UPCOMING EVENTS"
+            />
+            <MenuSelector
+                id="activities"
+                image={AGuy}
+                text="UPCOMING ACTIVITIES"
             />
         </div>
     );
--- frontend/src/components/MenuSelector.js
+++ frontend/src/components/MenuSelector.js
@@ -1,8 +1,13 @@
-const MenuSelector = ({ id, image, text }) => {
+const MenuSelector = ({ id, image, text, text2 }) => {
     return (
         <div id={id}>
             <img className="menu-image" src={image} alt="" />
             <p className="menu-text">{text}</p>
+            {text2 && (
+                <p id="text2" className="menu-text">
+                    {text}
+                </p>
+            )}
         </div>
     );
 };
