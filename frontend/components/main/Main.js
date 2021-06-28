6dc20ec88a78ae826387072fe9072ddcc4bef5f2
Parinz <parinzee@protonmail.com>
2021-06-28 13:35:00 +0700

    Save and commited

--- frontend/components/main/Main.js
+++ frontend/components/main/Main.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components/native";
 import Header from "./Header";
 import Menu from "./Menu"
+import { Asset } from "expo-asset";
 
 export default function Main() {
+
+    const fetchImages = () => {
+        const images = [
+            require("../../assets/glowingBlob.gif"),
+            require("../../assets/web.png"),
+            require("../../assets/100.png"),
+            require("../../assets/instagram.png"),
+            require("../../assets/tiktok.png"),
+            require("../../assets/line.png"),
+            require("../../assets/covid.png"),
+        ];
+
+        const cacheImages = images.map((image) => {
+            return Asset.fromModule(image).downloadAsync();
+        });
+
+        return Promise.all(cacheImages);
+    };
+
+    useEffect(() => fetchImages)
+
     const Container = styled.View`
         flex: 1;
         background-color: rgb(25, 25, 25);
@@ -14,7 +36,6 @@ export default function Main() {
         <Container>
             <Header />
             <Menu />
-
         </Container>
     );
 }
