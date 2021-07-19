a52ee3ade22d6bbefe2715655a9fccb9e5eb6e0e
Parinz <parinzee@protonmail.com>
2021-07-19 19:55:24 +0700

    Removed some old stuff

--- frontend/components/Pages/Me/Pages/Screen2.js
+++ frontend/components/Pages/Me/Pages/Screen2.js
@@ -4,9 +4,64 @@ import styled from "styled-components/native";
 export default function Screen2(navigation) {
 	const Container = styled.View`
 		flex: 1;
-		flex-direction: column;
 		background-color: rgb(25, 25, 25);
+		align-content: center;
+		justify-content: center;
 	`;
 
-	return <Container></Container>;
+	const InsideContainer = styled.View`
+		justify-content: center;
+		align-content: center;
+	`;
+
+	const Image = styled.Image`
+		width: ${moderateScale(170)}px;
+		height: ${moderateScale(170)}px;
+		align-self: center;
+	`;
+
+	const HiText = styled.Text`
+		text-align: center;
+		font-size: ${moderateScale(45)}px;
+		font-family: Now;
+		color: white;
+		margin-bottom: ${verticalScale(40)}px;
+	`;
+
+	const Description = styled.Text`
+		text-align: center;
+		font-size: ${moderateScale(20)}px;
+		font-family: Now;
+		color: white;
+		margin-vertical: ${verticalScale(10)}px;
+	`;
+
+	const handlePress = () => {
+		navigation.navigate("Screen2");
+	};
+
+	return (
+		<Container>
+			<InsideContainer>
+				<Image source={require("../../../../assets/drawing.png")} />
+				<HiText>Hey there!</HiText>
+				<Description>
+					Seems like this is your first time using the app! Please
+					allow us to sign you up!
+				</Description>
+				<TouchableOpacity onPress={handlePress}>
+					<FontAwesome5
+						name="arrow-circle-right"
+						size={50}
+						color="white"
+						style={{
+							alignSelf: "center",
+							marginBottom: verticalScale(100),
+							marginTop: verticalScale(30),
+						}}
+					/>
+				</TouchableOpacity>
+			</InsideContainer>
+		</Container>
+	);
 }
