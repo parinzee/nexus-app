e9cec8434da97190f30f0c9acf114f5f27b60ed6
Parinz <parinzee@protonmail.com>
2021-07-19 16:55:59 +0700

    Don't like them new header

--- frontend/App.js
+++ frontend/App.js
@@ -117,7 +117,7 @@ export default function App() {
 						name="Student Help"
 						component={TeamColor}
 						options={{
-							tabBarLabel: "Student Help",
+							tabBarLabel: "Student Tools",
 							tabBarIcon: ({ color, size }) => (
 								<FontAwesome5
 									name="hands-helping"
