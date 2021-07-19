78988dbbb49148f5a02328b46a8319edc23b0aa3
Parinz <parinzee@protonmail.com>
2021-07-19 16:25:33 +0700

    TeamColor.js

--- frontend/components/Pages/Content.js
+++ frontend/components/Pages/Content.js
@@ -5,149 +5,149 @@ import axios from "axios";
 import { moderateScale, verticalScale } from "react-native-size-matters";
 
 const Item = ({ eventName, eventDesc, mainColor }) => {
-    const Container = styled.View`
-        display: flex;
-        flex-direction: column;
-        background-color: ${mainColor};
-        border-radius: 20px;
-        width: ${moderateScale(295)}px;
-        height: ${moderateScale(100)}px;
-        margin-top: ${verticalScale(30)}px;
-        padding: 5px;
-        margin-right: 20px;
-        margin-left: 20px;
-        padding-right: 10px;
-    `;
+	const Container = styled.View`
+		display: flex;
+		flex-direction: column;
+		background-color: ${mainColor};
+		border-radius: 20px;
+		width: ${moderateScale(295)}px;
+		height: ${moderateScale(100)}px;
+		margin-top: ${verticalScale(30)}px;
+		padding: 5px;
+		margin-right: 20px;
+		margin-left: 20px;
+		padding-right: 10px;
+	`;
 
-    const TitleText = styled.Text`
-        color: white;
-        font-size: ${moderateScale(19)}px;
-        font-family: "OpenSans_800ExtraBold";
-        margin-left: 10px;
-        margin-bottom: 3px;
-    `;
+	const TitleText = styled.Text`
+		color: white;
+		font-size: ${moderateScale(19)}px;
+		font-family: "OpenSans_800ExtraBold";
+		margin-left: 10px;
+		margin-bottom: 3px;
+	`;
 
-    const SubtitleText = styled.Text`
-        color: white;
-        font-size: ${moderateScale(16)}px;
-        font-family: "OpenSans_300Light";
-        margin-left: 10px;
-    `;
-    return (
-        <Container>
-            <TitleText>{eventName}</TitleText>
-            <SubtitleText>{eventDesc}</SubtitleText>
-        </Container>
-    );
+	const SubtitleText = styled.Text`
+		color: white;
+		font-size: ${moderateScale(16)}px;
+		font-family: "OpenSans_300Light";
+		margin-left: 10px;
+	`;
+	return (
+		<Container>
+			<TitleText>{eventName}</TitleText>
+			<SubtitleText>{eventDesc}</SubtitleText>
+		</Container>
+	);
 };
 
 export default function BottomContent({ uri, mainColor }) {
-    const [items, setItems] = useState([]);
-    const [refresh, setRefresh] = useState(true);
-    const [error, setError] = useState(false);
+	const [items, setItems] = useState([]);
+	const [refresh, setRefresh] = useState(true);
+	const [error, setError] = useState(false);
 
-    const getEvents = async () => {
-        const data = await axios
-            .get(uri)
-            .then((response) => {
-                return response.data;
-            })
-            .catch(() => {
-                return false;
-            });
-        if (data === false) {
-            setError(true);
-            setRefresh(false);
-        } else {
-            setItems(data.sort((a, b) => b[0] - a[0]));
-            setRefresh(false);
-        }
-    };
-    useEffect(() => {
-        getEvents();
-    }, [refresh]);
+	const getEvents = async () => {
+		const data = await axios
+			.get(uri)
+			.then((response) => {
+				return response.data;
+			})
+			.catch(() => {
+				return false;
+			});
+		if (data === false) {
+			setError(true);
+			setRefresh(false);
+		} else {
+			setItems(data.sort((a, b) => b[0] - a[0]));
+			setRefresh(false);
+		}
+	};
+	useEffect(() => {
+		getEvents();
+	}, [refresh]);
 
-    const ListContainer = styled.View`
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        align-content: center;
-        margin-top: ${verticalScale(10)}px;
-        flex: 1;
-    `;
+	const ListContainer = styled.View`
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		align-content: center;
+		margin-top: ${verticalScale(10)}px;
+		flex: 1;
+	`;
 
-    const ModalContainer = styled.TouchableOpacity`
-        flex: 1;
-        align-content: center;
-        justify-content: center;
-        align-items: center;
-    `;
+	const ModalContainer = styled.TouchableOpacity`
+		flex: 1;
+		align-content: center;
+		justify-content: center;
+		align-items: center;
+	`;
 
-    const InnerContainer = styled.View`
-        background-color: rgb(25, 25, 25);
-        display: flex;
-        flex-direction: column;
-        border-width: 1px;
-        border-color: white;
-        border-radius: 25px;
-        width: 30%;
-        height: 17%;
-        padding: 10px;
-    `;
+	const InnerContainer = styled.View`
+		background-color: rgb(25, 25, 25);
+		display: flex;
+		flex-direction: column;
+		border-width: 1px;
+		border-color: white;
+		border-radius: 25px;
+		width: 30%;
+		height: 17%;
+		padding: 10px;
+	`;
 
-    const ModalTitle = styled.Text`
-        color: white;
-        font-family: System;
-        text-align: center;
-        font-size: ${moderateScale(25)}px;
-        margin-bottom: 10px;
-    `;
+	const ModalTitle = styled.Text`
+		color: white;
+		font-family: System;
+		text-align: center;
+		font-size: ${moderateScale(25)}px;
+		margin-bottom: 10px;
+	`;
 
-    const ModalText = styled.Text`
-        text-align: center;
-        color: white;
-        font-family: System;
-        font-size: ${moderateScale(13)}px;
-    `;
-    return (
-        <ListContainer>
-            <Modal
-                animationType="fade"
-                transparent={true}
-                visible={error}
-                onRequestClose={() => {
-                    setError(false);
-                }}
-            >
-                <ModalContainer
-                    onPress={() => {
-                        setError(false);
-                    }}
-                >
-                    <InnerContainer>
-                        <ModalTitle>Error</ModalTitle>
-                        <ModalText>
-                            Please check your internet connection.
-                        </ModalText>
-                    </InnerContainer>
-                </ModalContainer>
-            </Modal>
-            {refresh === false ? (
-                <FlatList
-                    data={items}
-                    renderItem={({ item }) => (
-                        <Item
-                            eventName={item[1]}
-                            eventDesc={item[2]}
-                            mainColor={mainColor}
-                        />
-                    )}
-                    keyExtractor={(entry) => entry[0].toString()}
-                />
-            ) : (
-                <ActivityIndicator size="large" color={mainColor} />
-            )}
-        </ListContainer>
-    );
+	const ModalText = styled.Text`
+		text-align: center;
+		color: white;
+		font-family: System;
+		font-size: ${moderateScale(13)}px;
+	`;
+	return (
+		<ListContainer>
+			<Modal
+				animationType="fade"
+				transparent={true}
+				visible={error}
+				onRequestClose={() => {
+					setError(false);
+				}}
+			>
+				<ModalContainer
+					onPress={() => {
+						setError(false);
+					}}
+				>
+					<InnerContainer>
+						<ModalTitle>Error</ModalTitle>
+						<ModalText>
+							Please check your internet connection.
+						</ModalText>
+					</InnerContainer>
+				</ModalContainer>
+			</Modal>
+			{refresh === false ? (
+				<FlatList
+					data={items}
+					renderItem={({ item }) => (
+						<Item
+							eventName={item[1]}
+							eventDesc={item[2]}
+							mainColor={mainColor}
+						/>
+					)}
+					keyExtractor={(entry) => entry[0].toString()}
+				/>
+			) : (
+				<ActivityIndicator size="large" color={mainColor} />
+			)}
+		</ListContainer>
+	);
 }
--- frontend/components/Pages/TeamColor/TeamColor.js
+++ frontend/components/Pages/TeamColor/TeamColor.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components/native";
 import Header from "../Header";
+import { LogBox } from "react-native";
 import Scores from "./Scores";
 import Content from "../Content";
 
@@ -8,18 +9,28 @@ export default function Activities({}) {
 	const Container = styled.View`
 		flex: 1;
 		background-color: rgb(25, 25, 25);
+		justify-content: center;
+		align-content: center;
 	`;
+
+	const AnotherContainer = styled.ScrollView``;
+
+	useEffect(() => {
+		LogBox.ignoreLogs(["VirtualizedLists should never be nested"]);
+	}, []);
 
 	return (
 		<Container>
-			<Scores
-				uri="http://nexussc.herokuapp.com/scores/"
-				mainColor="#ff9151"
-			/>
-			<Content
-				uri="http://nexussc.herokuapp.com/events/"
-				mainColor="#5071f6"
-			/>
+			<AnotherContainer>
+				<Scores
+					uri="http://nexussc.herokuapp.com/scores/"
+					mainColor="#ff9151"
+				/>
+				<Content
+					uri="http://nexussc.herokuapp.com/events/"
+					mainColor="#5071f6"
+				/>
+			</AnotherContainer>
 		</Container>
 	);
 }
