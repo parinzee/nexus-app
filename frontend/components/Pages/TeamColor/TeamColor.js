b4568c45dc5d6193dc549d88fd5dc86ceb0d197e
Parinz <parinzee@protonmail.com>
2021-07-19 16:05:33 +0700

    Merged scores into Team Color

--- frontend/components/Pages/TeamColor/TeamColor.js
+++ frontend/components/Pages/TeamColor/TeamColor.js
@@ -1,24 +1,25 @@
 import React from "react";
 import styled from "styled-components/native";
 import Header from "../Header";
+import Scores from "./Scores";
 import Content from "../Content";
 
-export default function Activities({ }) {
-    const Container = styled.View`
-        flex: 1;
-        background-color: rgb(25, 25, 25);
-    `;
+export default function Activities({}) {
+	const Container = styled.View`
+		flex: 1;
+		background-color: rgb(25, 25, 25);
+	`;
 
-    return (
-        <Container>
-            <Header
-                imagePath={require("../../../assets/AGuy.gif")}
-                text={`UPCOMING${"\n"}EVENTS`}
-                fontSize="28"
-                imageLeft={false}
-                margin="28"
-            />
-            <Content uri="http://nexussc.herokuapp.com/events/" mainColor="#5071f6" />
-        </Container>
-    );
+	return (
+		<Container>
+			<Scores
+				uri="http://nexussc.herokuapp.com/scores/"
+				mainColor="#ff9151"
+			/>
+			<Content
+				uri="http://nexussc.herokuapp.com/events/"
+				mainColor="#5071f6"
+			/>
+		</Container>
+	);
 }
--- frontend/components/Pages/competitions/Competitions.js
+++ frontend/components/Pages/competitions/Competitions.js
@@ -3,22 +3,21 @@ import styled from "styled-components/native";
 import Header from "../Header";
 import Content from "./Content";
 
-export default function Competitions({ }) {
-    const Container = styled.View`
-        flex: 1;
-        background-color: rgb(25, 25, 25);
-    `;
+export default function Competitions({}) {
+	const Container = styled.View`
+		flex: 1;
+		background-color: rgb(25, 25, 25);
+	`;
 
-    return (
-        <Container>
-            <Header
-                imagePath={require("../../../assets/teacher.gif")}
-                text={`TEAM COLOR${"\n"}SCORES`}
-                fontSize="25"
-                imageLeft={true}
-                margin="22"
-            />
-            <Content uri="http://nexussc.herokuapp.com/scores/" mainColor="#ff9151" />
-        </Container>
-    );
+	return (
+		<Container>
+			<Header
+				imagePath={require("../../../assets/teacher.gif")}
+				text={`TEAM COLOR${"\n"}SCORES`}
+				fontSize="25"
+				imageLeft={true}
+				margin="22"
+			/>
+		</Container>
+	);
 }
