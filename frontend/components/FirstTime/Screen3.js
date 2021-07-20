dce5cf374e50f2d1516978ca89c82a2734157c21
Parinz <parinzee@protonmail.com>
2021-07-20 10:06:44 +0700

    New Privacy Policy

--- frontend/components/FirstTime/Screen3.js
+++ frontend/components/FirstTime/Screen3.js
@@ -1,16 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	TouchableOpacity,
 	KeyboardAvoidingView,
 	Platform,
 	TouchableWithoutFeedback,
 	Keyboard,
+	View,
 } from "react-native";
 import styled from "styled-components/native";
 import { verticalScale, moderateScale } from "react-native-size-matters";
 import { FontAwesome5 } from "@expo/vector-icons";
 import LottieView from "lottie-react-native";
 import { Input } from "react-native-elements";
+import { Picker } from "@react-native-picker/picker";
+
+const LogicPart = () => {
+	const [teamColor, setTeamColor] = useState();
+	return (
+		<View>
+			<Input
+				placeholder="Nickname (eg: John)"
+				leftIcon={
+					<FontAwesome5 name="address-book" size={24} color="white" />
+				}
+				inputContainerStyle={{
+					marginTop: verticalScale(20),
+					marginLeft: moderateScale(20),
+					marginRight: moderateScale(20),
+				}}
+				inputStyle={{ color: "white" }}
+				maxLength={10}
+			/>
+			<Input
+				placeholder=" Grade (eg: 07)"
+				leftIcon={
+					<FontAwesome5 name="address-card" size={24} color="white" />
+				}
+				inputContainerStyle={{
+					marginBottom: verticalScale(-30),
+					marginLeft: moderateScale(20),
+					marginRight: moderateScale(20),
+				}}
+				maxLength={2}
+				inputStyle={{ color: "white" }}
+				keyboardType="numeric"
+			/>
+			<Picker
+				selectedValue={teamColor}
+				onValueChange={(itemValue, itemIndex) =>
+					setTeamColor(itemValue)
+				}
+				itemStyle={{ color: "white" }}
+			>
+				<Picker.Item label="Red Team" value="red" />
+				<Picker.Item label="Blue Team" value="blue" />
+				<Picker.Item label="Green Team" value="green" />
+				<Picker.Item label="Yellow Team" value="yellow" />
+			</Picker>
+			<TouchableOpacity>
+				<FontAwesome5
+					name="arrow-circle-right"
+					size={50}
+					color="white"
+					style={{
+						alignSelf: "center",
+					}}
+				/>
+			</TouchableOpacity>
+		</View>
+	);
+};
 
 export default function Screen3() {
 	const Container = styled.View`
@@ -28,7 +87,7 @@ export default function Screen3() {
 
 	const HiText = styled.Text`
 		text-align: center;
-		font-size: ${moderateScale(45)}px;
+		font-size: ${moderateScale(30)}px;
 		font-family: Now;
 		color: white;
 		margin-bottom: ${verticalScale(20)}px;
@@ -55,8 +114,8 @@ export default function Screen3() {
 							speed={0.7}
 							style={{
 								position: "relative",
-								width: moderateScale(200),
-								height: moderateScale(200),
+								width: moderateScale(150),
+								height: moderateScale(150),
 								alignSelf: "center",
 							}}
 						/>
@@ -66,32 +125,7 @@ export default function Screen3() {
 							you for a few things! (Name, team color, and grade)
 						</Description>
 
-						<Input
-							placeholder="Name (Nickname)"
-							leftIcon={
-								<FontAwesome5
-									name="address-book"
-									size={24}
-									color="white"
-								/>
-							}
-							inputContainerStyle={{
-								marginTop: verticalScale(20),
-								marginLeft: moderateScale(20),
-								marginRight: moderateScale(20),
-							}}
-						/>
-						<TouchableOpacity>
-							<FontAwesome5
-								name="arrow-circle-right"
-								size={50}
-								color="white"
-								style={{
-									alignSelf: "center",
-									marginTop: verticalScale(30),
-								}}
-							/>
-						</TouchableOpacity>
+						<LogicPart />
 					</InsideContainer>
 				</TouchableWithoutFeedback>
 			</KeyboardAvoidingView>
