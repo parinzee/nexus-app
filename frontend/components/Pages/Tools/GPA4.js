ffd533db03998d2bc27141195a1f5094ba3dfc0a
Parinz <parinzee@protonmail.com>
2021-07-20 20:11:44 +0700

    I think this logic works?

--- frontend/components/Pages/Tools/GPA4.js
+++ frontend/components/Pages/Tools/GPA4.js
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
 import { WebView } from "react-native-webview";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { Alert } from "react-native";
 
 const Standards = () => {
 	const HTML = require("../../../assets/Standards.html");
@@ -18,11 +19,59 @@ const Standards = () => {
 };
 
 export default function GPA4({ route }) {
+	const [honors, setHonors] = useState();
 	const { grade } = route.params;
 	const Container = styled.View`
 		flex: 1;
 		background-color: rgb(35, 35, 35);
 	`;
 
-	return <Container>{grade < 9 ? <Standards /> : <Standards />}</Container>;
+	useEffect(() => {
+		const CheckStandards = async () => {
+			const value = await AsyncStorage.getItem("@honors");
+			if (value === null) {
+				Alert.alert(
+					"Are you in honors?",
+					"We need this to accurately determine your grade.",
+					[
+						{
+							text: "Yes",
+							onPress: async () => {
+								await AsyncStorage.setItem(
+									"@honors",
+									JSON.stringify(true)
+								);
+								setHonors(true);
+							},
+						},
+						{
+							text: "No",
+							onPress: async () => {
+								await AsyncStorage.setItem(
+									"@honors",
+									JSON.stringify(false)
+								);
+								setHonors(false);
+							},
+						},
+					]
+				);
+			} else {
+				return JSON.parse(value);
+			}
+		};
+
+		CheckStandards();
+	}, [honors]);
+	return (
+		<Container>
+			{grade < 9 ? (
+				<Standards />
+			) : honors === false ? (
+				<Standards />
+			) : (
+				<Container />
+			)}
+		</Container>
+	);
 }
