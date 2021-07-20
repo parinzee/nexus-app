ef734d342ba7476df3a40e70310e031daf20f817
Parinz <parinzee@protonmail.com>
2021-07-20 19:49:32 +0700

    Implemented Standards.html

--- frontend/components/Pages/Tools/GPA4.js
+++ frontend/components/Pages/Tools/GPA4.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
+import { WebView } from "react-native-webview";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export default function GPA4({ navigation, grade }) {
+const Standards = () => {
+	const HTML = require("../../../assets/Standards.html");
+	const handleGPA = async (grade) => {
+		await AsyncStorage.setItem("@GPA", JSON.stringify(grade));
+	};
+	return (
+		<WebView
+			style={{ flex: 1 }}
+			source={HTML}
+			onMessage={(event) => handleGPA(event.nativeEvent.data)}
+		/>
+	);
+};
+
+export default function GPA4({ route }) {
+	const { grade } = route.params;
 	const Container = styled.View`
 		flex: 1;
 		background-color: rgb(35, 35, 35);
 	`;
 
-	return <Container></Container>;
+	return <Container>{grade < 9 ? <Standards /> : <Standards />}</Container>;
 }
--- frontend/components/Pages/Tools/Tools.js
+++ frontend/components/Pages/Tools/Tools.js
@@ -1,18 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { TouchableOpacity, Animated } from "react-native";
+import { TouchableOpacity } from "react-native";
 import styled from "styled-components/native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createStackNavigator } from "@react-navigation/stack";
 import { ListItem } from "react-native-elements";
 import { FontAwesome5 } from "@expo/vector-icons";
 import Header from "../Header";
 import { verticalScale, moderateScale } from "react-native-size-matters";
 
-const Stack = createStackNavigator();
-
 export default function Tools1({ navigation }) {
 	const [grade, setGrade] = useState(0);
-	const DestinationsHigh = [
+	const Destinations = [
 		{
 			title: "Calculate Grades to 4.0",
 			icon: (
@@ -23,30 +20,6 @@ export default function Tools1({ navigation }) {
 				/>
 			),
 			destination: "Calculate to 4.0",
-		},
-		{
-			title: "My notes",
-			icon: (
-				<FontAwesome5
-					name="clipboard-list"
-					size={moderateScale(24)}
-					color="white"
-				/>
-			),
-			destination: "Todo",
-		},
-	];
-	const Destinations = [
-		{
-			title: "Calculate Grades to 100",
-			icon: (
-				<FontAwesome5
-					name="calculator"
-					size={moderateScale(24)}
-					color="white"
-				/>
-			),
-			destination: "GPA100",
 		},
 		{
 			title: "My notes",
@@ -80,61 +53,29 @@ export default function Tools1({ navigation }) {
 		<Container>
 			<Header text="Tools" fontSize="35" />
 			<BottomContainer>
-				{grade < 7
-					? Destinations.map(
-							({ title, icon, destination }, index) => (
-								<TouchableOpacity
-									onPress={() =>
-										navigation.navigate(destination, {})
-									}
-								>
-									<ListItem
-										key={index}
-										bottomDivider
-										containerStyle={{
-											backgroundColor: "rgb(25,25,25)",
-										}}
-									>
-										{icon}
-										<ListItem.Content>
-											<ListItem.Title
-												style={{ color: "white" }}
-											>
-												{title}
-											</ListItem.Title>
-										</ListItem.Content>
-										<ListItem.Chevron />
-									</ListItem>
-								</TouchableOpacity>
-							)
-					  )
-					: DestinationsHigh.map(
-							({ title, icon, destination }, index) => (
-								<TouchableOpacity
-									onPress={() =>
-										navigation.navigate(destination)
-									}
-								>
-									<ListItem
-										key={index}
-										bottomDivider
-										containerStyle={{
-											backgroundColor: "rgb(25,25,25)",
-										}}
-									>
-										{icon}
-										<ListItem.Content>
-											<ListItem.Title
-												style={{ color: "white" }}
-											>
-												{title}
-											</ListItem.Title>
-										</ListItem.Content>
-										<ListItem.Chevron />
-									</ListItem>
-								</TouchableOpacity>
-							)
-					  )}
+				{Destinations.map(({ title, icon, destination }, index) => (
+					<TouchableOpacity
+						onPress={() =>
+							navigation.navigate(destination, { grade })
+						}
+					>
+						<ListItem
+							key={index}
+							bottomDivider
+							containerStyle={{
+								backgroundColor: "rgb(25,25,25)",
+							}}
+						>
+							{icon}
+							<ListItem.Content>
+								<ListItem.Title style={{ color: "white" }}>
+									{title}
+								</ListItem.Title>
+							</ListItem.Content>
+							<ListItem.Chevron />
+						</ListItem>
+					</TouchableOpacity>
+				))}
 			</BottomContainer>
 		</Container>
 	);
