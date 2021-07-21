70342f2023c38276b1f5c4a51bb24c87ca34b60e
Parinz <parinzee@protonmail.com>
2021-07-21 17:50:39 +0700

    Finished Todo

--- frontend/components/Pages/Tools/Todo.js
+++ frontend/components/Pages/Tools/Todo.js
@@ -9,14 +9,19 @@ import styled from "styled-components";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MaterialIcons } from "@expo/vector-icons";
 import { moderateScale, verticalScale } from "react-native-size-matters";
-import { Keyboard, TouchableWithoutFeedback } from "react-native";
-import BottomSheet, { BottomSheetTextInput } from "@gorhom/bottom-sheet";
-import { Input } from "react-native-elements";
+import {
+	Keyboard,
+	TouchableWithoutFeedback,
+	View,
+	ScrollView,
+	TextInput,
+} from "react-native";
+import { Header } from "@react-navigation/stack";
 import { LinearGradient } from "expo-linear-gradient";
-import { FlatList } from "react-native";
-import { render } from "react-dom";
+import { useHeaderHeight } from "@react-navigation/stack";
 
-const Task = ({ title, isChecked, setCheck, setRemove }) => {
+const Task = ({ title, isChecked, indexProp, setCheck, setRemove }) => {
+	const [index, setIndex] = useState(indexProp);
 	const TaskContainer = styled.View`
 		width: 85%;
 		background-color: #17181f;
@@ -43,7 +48,7 @@ const Task = ({ title, isChecked, setCheck, setRemove }) => {
 
 	return (
 		<TaskContainer>
-			<Inner>
+			<Inner onPress={() => setCheck(index)}>
 				<MaterialIcons
 					name={isChecked ? "check-box" : "check-box-outline-blank"}
 					size={24}
@@ -56,96 +61,96 @@ const Task = ({ title, isChecked, setCheck, setRemove }) => {
 				size={24}
 				color="#F977A1"
 				style={{ alignSelf: "center", marginRight: moderateScale(10) }}
+				onPress={() => setRemove(index)}
 			/>
 		</TaskContainer>
 	);
 };
 
 const AddTodos = ({ HandleAddTask }) => {
-	const [todo, setTodo] = useState("Walk the dog");
+	const [todo, setTodo] = useState("");
+	const height = useHeaderHeight();
 
 	const textInputRef = useRef(null);
 
 	// ref
-	const bottomSheetRef = useRef(null);
-
-	// variables
-	const snapPoints = useMemo(() => ["5%", "10%"], []);
-
-	// callbacks
-	const handleSheetChanges = useCallback((index) => {
-		console.log("handleSheetChanges", index);
-	}, []);
-
-	const Background = styled.View`
-		background-color: rgb(50, 50, 50);
-	`;
-	const Container = styled.View`
-		flex: 1;
+	const Container = styled.KeyboardAvoidingView`
 		flex-direction: row;
+		margin-bottom: 30px;
 	`;
 
 	return (
-		<BottomSheet
-			ref={bottomSheetRef}
-			index={1}
-			snapPoints={snapPoints}
-			onChange={handleSheetChanges}
-			backgroundComponent={Background}
-			keyboardBehavior="interactive"
-			keyboardBlurBehavior="restore"
+		<Container
+			behavior={Platform.OS === "ios" ? "padding" : "height"}
+			keyboardVerticalOffset={height + 20}
 		>
-			<Container>
-				<BottomSheetTextInput
-					ref={textInputRef}
-					placeholder="Add your task/notes here!"
-					placeholderTextColor="grey"
-					style={{
-						borderRadius: 10,
-						width: "80%",
-						height: verticalScale(40),
-						borderColor: "white",
-						borderWidth: 1,
-						marginLeft: moderateScale(10),
-						paddingLeft: moderateScale(5),
-						color: "white",
-					}}
-					onChangeText={(text) => {
-						textInputRef.current.value = text;
-					}}
+			<TextInput
+				ref={textInputRef}
+				placeholder="Add your task/notes here!"
+				placeholderTextColor="grey"
+				style={{
+					borderRadius: 10,
+					width: "80%",
+					height: verticalScale(40),
+					borderColor: "white",
+					borderWidth: 1,
+					marginLeft: moderateScale(10),
+					paddingLeft: moderateScale(5),
+					color: "white",
+				}}
+				onChangeText={(text) => {
+					textInputRef.current.value = text;
+				}}
+			/>
+			<LinearGradient
+				colors={["#CA4AD0", "#FC9187"]}
+				style={{
+					borderRadius: 15,
+					marginLeft: 10,
+					width: "10%",
+					height: verticalScale(40),
+					alignContent: "center",
+					justifyContent: "center",
+				}}
+			>
+				<MaterialIcons
+					name="add"
+					size={moderateScale(28)}
+					color="white"
+					style={{ alignSelf: "center" }}
+					onPress={() => HandleAddTask(textInputRef.current.value)}
 				/>
-				<LinearGradient
-					colors={["#CA4AD0", "#FC9187"]}
-					style={{
-						borderRadius: 15,
-						marginLeft: 10,
-						width: "10%",
-						height: verticalScale(40),
-						alignContent: "center",
-						justifyContent: "center",
-					}}
-				>
-					<MaterialIcons
-						name="add"
-						size={moderateScale(28)}
-						color="white"
-						style={{ alignSelf: "center" }}
-						onPress={() =>
-							HandleAddTask(textInputRef.current.value)
-						}
-					/>
-				</LinearGradient>
-			</Container>
-		</BottomSheet>
+			</LinearGradient>
+		</Container>
 	);
 };
 
+function arrayRemove(arr, value) {
+	return arr.filter((v, index) => {
+		return index != value;
+	});
+}
+
+const MemoizedTodo = React.memo(AddTodos, (prev, next) => false);
+const TodoList = ({ tasks, HandleCheckTask, HandleRemoveTask }) => {
+	return (
+		<View onStartShouldSetResponder={() => true}>
+			{tasks.map(({ title, isChecked }, index) => {
+				return (
+					<Task
+						title={title}
+						isChecked={isChecked}
+						indexProp={index}
+						setCheck={HandleCheckTask}
+						setRemove={HandleRemoveTask}
+					/>
+				);
+			})}
+		</View>
+	);
+};
 export default function Todo() {
 	const [tasks, setTasks] = useState([]);
-
-	const renderItem = ({ item }) => (
-		<Task title={item.title} isChecked={item.isChecked} />
-	);
 
 	useEffect(() => {
 		async function ReadTasks() {
@@ -159,12 +164,12 @@ export default function Todo() {
 		ReadTasks();
 	}, []);
 
-	const HandleAddTask = async (task) => {
+	const HandleAddTask = useCallback(async (task) => {
 		const newTask = { title: task, isChecked: false };
 		const newArr = [...tasks, newTask];
 		await AsyncStorage.setItem("@tasks", JSON.stringify(newArr));
 		setTasks(newArr);
-	};
+	});
 
 	const Container = styled.View`
 		flex: 1;
@@ -172,11 +177,32 @@ export default function Todo() {
 		justify-content: flex-end;
 	`;
 
+	const HandleRemoveTask = async (id) => {
+		const taskCopy = arrayRemove(tasks, id);
+		setTasks(taskCopy);
+		await AsyncStorage.setItem("@tasks", JSON.stringify(taskCopy));
+	};
+
+	const HandleCheckTask = async (index) => {
+		let newTask = tasks[index];
+		let newTasks = [...tasks];
+		newTask["isChecked"] = !newTask["isChecked"];
+		newTasks.splice(index, 1, newTask);
+		setTasks(newTasks);
+		await AsyncStorage.setItem("@tasks", JSON.stringify(newTasks));
+	};
+
 	return (
 		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
 			<Container>
-				<FlatList data={tasks} renderItem={renderItem} />
-				<AddTodos HandleAddTask={HandleAddTask} />
+				<ScrollView style={{ flex: 1 }}>
+					<TodoList
+						tasks={tasks}
+						HandleCheckTask={HandleCheckTask}
+						HandleRemoveTask={HandleRemoveTask}
+					/>
+				</ScrollView>
+				<MemoizedTodo HandleAddTask={HandleAddTask} />
 			</Container>
 		</TouchableWithoutFeedback>
 	);
