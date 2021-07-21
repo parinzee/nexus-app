db29e074d509c45598f931ee3b1efdc41197c7d1
Parinz <parinzee@protonmail.com>
2021-07-21 14:29:22 +0700

    Created bottom sheet

--- frontend/components/Pages/Tools/Todo.js
+++ frontend/components/Pages/Tools/Todo.js
@@ -9,8 +9,15 @@ import styled from "styled-components";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MaterialIcons } from "@expo/vector-icons";
 import { moderateScale, verticalScale } from "react-native-size-matters";
-import { KeyboardAvoidingView, Keyboard } from "react-native";
-import BottomSheet from "@gorhom/bottom-sheet";
+import {
+	KeyboardAvoidingView,
+	Keyboard,
+	TouchableWithoutFeedback,
+} from "react-native";
+import BottomSheet, { BottomSheetTextInput } from "@gorhom/bottom-sheet";
+import { Input } from "react-native-elements";
+import { LinearGradient } from "expo-linear-gradient";
+import { Pressable } from "react-native";
 
 const Task = ({ todo, setCheck, setRemove }) => {
 	const { title, isChecked } = todo;
@@ -45,26 +52,87 @@ const Task = ({ todo, setCheck, setRemove }) => {
 	);
 };
 
-const AddTodos = () => {
-	return (
-		<KeyboardAvoidingView
-			behavior={Platform.OS === "ios" ? "padding" : "height"}
-		></KeyboardAvoidingView>
-	);
-};
+const AddTodos = ({ HandleAddTask }) => {
+	const [todo, setTodo] = useState("Walk the dog");
 
-export default function Todo() {
-	const [tasks, setTasks] = useState([]);
+	const textInputRef = useRef(null);
+
 	// ref
-	const bottomSheetRef = useRef < BottomSheet > null;
+	const bottomSheetRef = useRef(null);
 
 	// variables
-	const snapPoints = useMemo(() => ["25%", "50%"], []);
+	const snapPoints = useMemo(() => ["5%", "10%"], []);
 
 	// callbacks
 	const handleSheetChanges = useCallback((index) => {
 		console.log("handleSheetChanges", index);
 	}, []);
+
+	const Background = styled.View`
+		background-color: rgb(50, 50, 50);
+	`;
+	const Container = styled.View`
+		flex: 1;
+		flex-direction: row;
+	`;
+
+	return (
+		<BottomSheet
+			ref={bottomSheetRef}
+			index={1}
+			snapPoints={snapPoints}
+			onChange={handleSheetChanges}
+			backgroundComponent={Background}
+			keyboardBehavior="interactive"
+			keyboardBlurBehavior="restore"
+		>
+			<Container>
+				<BottomSheetTextInput
+					ref={textInputRef}
+					placeholder="Add your task/notes here!"
+					placeholderTextColor="grey"
+					style={{
+						borderRadius: 10,
+						width: "80%",
+						height: verticalScale(40),
+						borderColor: "white",
+						borderWidth: 1,
+						marginLeft: moderateScale(10),
+						paddingLeft: moderateScale(5),
+						color: "white",
+					}}
+					onChangeText={(text) => {
+						textInputRef.current.value = text;
+					}}
+				/>
+				<LinearGradient
+					colors={["#CA4AD0", "#FC9187"]}
+					style={{
+						borderRadius: 15,
+						marginLeft: 10,
+						width: "10%",
+						height: verticalScale(40),
+						alignContent: "center",
+						justifyContent: "center",
+					}}
+				>
+					<MaterialIcons
+						name="add"
+						size={moderateScale(28)}
+						color="white"
+						style={{ alignSelf: "center" }}
+						onPress={() =>
+							HandleAddTask(textInputRef.current.value)
+						}
+					/>
+				</LinearGradient>
+			</Container>
+		</BottomSheet>
+	);
+};
+
+export default function Todo() {
+	const [tasks, setTasks] = useState([]);
 
 	useEffect(() => {
 		async function ReadTasks() {
@@ -79,7 +147,8 @@ export default function Todo() {
 	}, []);
 
 	const HandleAddTask = async (task) => {
-		const newArr = [...tasks, task];
+		const newTask = { title: task, isChecked: false };
+		const newArr = [...tasks, newTask];
 		await AsyncStorage.setItem("@tasks", JSON.stringify(newArr));
 		setTasks(newArr);
 	};
@@ -87,11 +156,14 @@ export default function Todo() {
 	const Container = styled.View`
 		flex: 1;
 		background-color: rgb(35, 35, 35);
+		justify-content: flex-end;
 	`;
 
 	return (
-		<Container>
-			<BottomSheet snapPoints={snapPoints} />
-		</Container>
+		<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+			<Container>
+				<AddTodos HandleAddTask={HandleAddTask} />
+			</Container>
+		</TouchableWithoutFeedback>
 	);
 }
