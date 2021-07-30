4205c00fb344d781c4bd49556dc3f34e89ff8907
Parinz <parinzee@protonmail.com>
2021-07-30 09:29:46 +0700

    Basis for push-notifications

--- frontend/App.js
+++ frontend/App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import AppLoading from "expo-app-loading";
+import * as Notifications from "expo-notifications";
 import {
 	useFonts,
 	OpenSans_800ExtraBold,
