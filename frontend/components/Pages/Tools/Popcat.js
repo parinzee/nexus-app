3580796710b10396800f406e561625c3abd4de54
Parinz <parinzee@protonmail.com>
2021-09-21 19:23:15 +0700

    Cat images!

--- frontend/components/Pages/Tools/Popcat.js
+++ frontend/components/Pages/Tools/Popcat.js
@@ -1,10 +1,29 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
+import { moderateScale, verticalScale } from "react-native-size-matters";
 
 export default function PopCat() {
-	const Cat = styled.Image`
+	const [clicked, setClicked] = useState(false);
+	const image = [
+		require("../../../assets/Cat-Closed.png"),
+		require("../../../assets/Cat-Open.png"),
+	];
+
+	const Container = styled.View`
 		flex: 1;
+		background-color: #121212;
+		justify-content: center;
+		align-items: center;
 	`;
 
-    return <Cat />
+	const Cat = styled.Image`
+		height: ${moderateScale(400)}px;
+		width: ${moderateScale(400)}px;
+	`;
+
+	return (
+		<Container>
+			<Cat source={clicked === false ? image[0] : image[1]} />
+		</Container>
+	);
 }
