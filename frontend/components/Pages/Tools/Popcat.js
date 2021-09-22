84537fde575db41c73b633b55508e70f6d5f34f6
Parinz <parinzee@protonmail.com>
2021-09-22 08:29:25 +0700

    Added rudimentary counter

--- frontend/components/Pages/Tools/Popcat.js
+++ frontend/components/Pages/Tools/Popcat.js
@@ -1,17 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Pressable } from "react-native";
+import React, { useState } from "react";
 import styled from "styled-components/native";
 import { moderateScale } from "react-native-size-matters";
 import { Audio } from "expo-av";
 
 export default function PopCat() {
-	const [clicked, setClicked] = useState(false);
-	const [sound, setSound] = useState(new Audio.Sound());
-	const imgsrc = [
-		require("../../../assets/Cat-Closed.png"),
-		require("../../../assets/Cat-Open.png"),
-	];
-
 	const Container = styled.View`
 		flex: 1;
 		background-color: #121212;
@@ -19,9 +11,29 @@ export default function PopCat() {
 		align-items: center;
 	`;
 
+	return (
+		<Container>
+			<Cat />
+		</Container>
+	);
+}
+
+function Cat() {
+	const [clicked, setClicked] = useState(false);
+	const [clicks, setClicks] = useState(0);
+	const imgsrc = [
+		require("../../../assets/Cat-Closed.png"),
+		require("../../../assets/Cat-Open.png"),
+	];
+
 	const Cat = styled.Image`
 		height: ${moderateScale(400)}px;
 		width: ${moderateScale(400)}px;
+	`;
+
+	const Pressable = styled.Pressable`
+		display: flex;
+		flex-direction: column;
 	`;
 
 	async function playSound() {
@@ -35,6 +47,7 @@ export default function PopCat() {
 		playSound();
 		setTimeout(() => {
 			setClicked(false);
+			setClicks(clicks + 1);
 		}, 40);
 	};
 
@@ -43,18 +56,29 @@ export default function PopCat() {
 		playSound();
 		setTimeout(() => {
 			setClicked(false);
+			setClicks(clicks + 1);
 		}, 400);
 	};
 
 	return (
-		<Container>
-			<Pressable
-				onPress={onPress}
-				onLongPress={onLongPress}
-				delayLongPress={200}
-			>
-				<Cat source={clicked === false ? imgsrc[0] : imgsrc[1]} />
-			</Pressable>
-		</Container>
+		<Pressable
+			onPress={onPress}
+			onLongPress={onLongPress}
+			delayLongPress={200}
+		>
+			<Counter clicks={clicks} />
+			<Cat source={clicked === false ? imgsrc[0] : imgsrc[1]} />
+		</Pressable>
 	);
+}
+
+function Counter({ clicks }) {
+	const CounterText = styled.Text`
+		font-family: System;
+		font-size: ${moderateScale(50)}px;
+		color: white;
+		text-align: center;
+	`;
+
+	return <CounterText>{clicks}</CounterText>;
 }
