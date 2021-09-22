a29290a7cfae5ea4b8d755cf6ab5197aac4c6b91
Parinz <parinzee@protonmail.com>
2021-09-22 08:08:53 +0700

    Clicking baby!

--- frontend/components/Pages/Tools/Clicker.js
+++ frontend/components/Pages/Tools/Clicker.js
@@ -1,61 +1,71 @@
-import React, { useEffect, useState } from "react"
-import {Vibration, Alert, TouchableOpacity} from "react-native"
-import styled from "styled-components/native"
-import AsyncStorage from "@react-native-async-storage/async-storage"
-import { moderateScale, verticalScale } from "react-native-size-matters"
-import {Audio} from "expo-av"
+import React, { useEffect, useState } from "react";
+import { Vibration, Alert, TouchableOpacity } from "react-native";
+import styled from "styled-components/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { moderateScale, verticalScale } from "react-native-size-matters";
+import { Audio } from "expo-av";
 
 export default function Clicker() {
-  const [clicks, setClicks] = useState(0)
+	const [clicks, setClicks] = useState(0);
 
-  const Container = styled.View`
-    flex: 1;
-    background-color: #121212;
-    justify-content: center;
-    align-items: center;
-  `
-  const Counter = styled.Text`
-    font-family: System;
-    color: white;
-    font-size: ${moderateScale(30)}px;
-    position: absolute;
-    bottom: ${verticalScale(10)}px;
-  `
-  async function playSound() {
-    const { sound } = await Audio.Sound.createAsync(
-       require('../../../assets/click.mp3')
-    );
-    sound.playAsync()
-  }
+	const Container = styled.View`
+		flex: 1;
+		background-color: #121212;
+		justify-content: center;
+		align-items: center;
+	`;
+	const Counter = styled.Text`
+		font-family: System;
+		color: white;
+		font-size: ${moderateScale(30)}px;
+		position: absolute;
+		bottom: ${verticalScale(10)}px;
+	`;
+	async function playSound() {
+		const { sound } = await Audio.Sound.createAsync(
+			require("../../../assets/click.mp3")
+		);
+		sound.playAsync();
+	}
 
-  const handlePress = async () => {
-    Vibration.vibrate()
-    playSound()
-    if (clicks >= 1000) {
-      setClicks(0)
-      AsyncStorage.setItem("@counter", JSON.stringify(0))
-    } else {
-      setClicks(clicks + 1)
-      AsyncStorage.setItem("@counter", JSON.stringify(clicks + 1))
-    }
-  }
+	const handlePress = async () => {
+		Vibration.vibrate();
+		playSound();
+		if (clicks >= 1000) {
+			setClicks(0);
+			AsyncStorage.setItem("@counter", JSON.stringify(0));
+		} else {
+			setClicks(clicks + 1);
+			AsyncStorage.setItem("@counter", JSON.stringify(clicks + 1));
+		}
+	};
 
-  useEffect(() => {
-    async function getCounts() {
-      const counts = JSON.parse(await AsyncStorage.getItem("@counter"))
-      if (counts === null) {
-        Alert.alert("Sound", "Please turn sound on.", [{text: "Ok"}])
-      } else {
-        setClicks(counts)
-      }
-    }
-    getCounts()
-  }, [])
+	useEffect(() => {
+		async function getCounts() {
+			const counts = JSON.parse(await AsyncStorage.getItem("@counter"));
+			if (counts === null) {
+				Alert.alert("Sound", "Please turn sound on.", [{ text: "Ok" }]);
+			} else {
+				setClicks(counts);
+			}
+		}
+		getCounts();
+	}, []);
 
-  return (
-    <Container>
-      <TouchableOpacity activeOpacity={1} onPress={handlePress} style={{ width: moderateScale(200) - (0.2 * clicks), height: moderateScale(200) - (0.2 * clicks), backgroundColor: "#CA8A8B",alignSelf: "center", borderRadius: clicks*0.1}} />
-      <Counter>{clicks}</Counter>
-    </Container>
-  )
+	return (
+		<Container>
+			<TouchableOpacity
+				activeOpacity={1}
+				onPress={handlePress}
+				style={{
+					width: moderateScale(200) - 0.2 * clicks,
+					height: moderateScale(200) - 0.2 * clicks,
+					backgroundColor: "#CA8A8B",
+					alignSelf: "center",
+					borderRadius: clicks * 0.1,
+				}}
+			/>
+			<Counter>{clicks}</Counter>
+		</Container>
+	);
 }
--- frontend/components/Pages/Tools/Popcat.js
+++ frontend/components/Pages/Tools/Popcat.js
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { Pressable } from "react-native";
 import styled from "styled-components/native";
-import { moderateScale, verticalScale } from "react-native-size-matters";
+import { moderateScale } from "react-native-size-matters";
+import { Audio } from "expo-av";
 
 export default function PopCat() {
 	const [clicked, setClicked] = useState(false);
+	const [sound, setSound] = useState(new Audio.Sound());
 	const imgsrc = [
 		require("../../../assets/Cat-Closed.png"),
 		require("../../../assets/Cat-Open.png"),
@@ -22,18 +24,26 @@ export default function PopCat() {
 		width: ${moderateScale(400)}px;
 	`;
 
-	const onPress = () => {
+	async function playSound() {
+		const { sound } = await Audio.Sound.createAsync(
+			require("../../../assets/click.mp3")
+		);
+		sound.playAsync();
+	}
+	const onPress = async () => {
 		setClicked(true);
+		playSound();
 		setTimeout(() => {
 			setClicked(false);
-		}, 30);
+		}, 40);
 	};
 
-	const onLongPress = () => {
+	const onLongPress = async () => {
 		setClicked(true);
+		playSound();
 		setTimeout(() => {
 			setClicked(false);
-		}, 500);
+		}, 400);
 	};
 
 	return (
