33813c74ffa461dcd1886b4bfc9095d689d57697
Parinz <parinzee@protonmail.com>
2021-09-23 19:35:05 +0700

    Redis Modulo 3 to not kill it

--- backend/rediswrapper.py
+++ backend/rediswrapper.py
@@ -9,5 +9,6 @@ async def increment_score(data: dict):
     deviceID, name, team, clicks = itemgetter("deviceID", "name", "team", "clicks")(
         data
     )
-    await r.zincrby("teams", 1, team)
-    await r.zadd("individual", {f"{deviceID}:{name}": float(clicks)})
+    if int(clicks) % 3 == 0:
+        await r.zincrby("teams", 3, team)
+        await r.zadd("individual", {f"{deviceID}:{name}": float(clicks)})
