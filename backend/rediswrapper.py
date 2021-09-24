135c37cf660f1fbfda59ba32e44f737064cba318
Parinz <parinzee@protonmail.com>
2021-09-24 16:54:15 +0700

    Fixed minor typo that caused app crash

--- backend/rediswrapper.py
+++ backend/rediswrapper.py
@@ -28,8 +28,9 @@ async def increment_score(data: dict):
 async def get_leaderboard():
     teamLeaderboard = await r.zrange("teams", 0, -1, True, True)
     individualLeaderboard = await r.zrange("individual", 0, -1, True, True)
+    # TODO
 
     return {
-        "t": list(map(lambda x: [x[0].split(":")[1], x[1]], teamLeaderboard)),
-        "i": individualLeaderboard,
+        "i": list(map(lambda x: [x[0].split(":")[1], x[1]], individualLeaderboard)),
+        "t": teamLeaderboard,
     }
