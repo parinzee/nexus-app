07f74007561e1b1393a120b275d5fda19f69eb5d
Parinz <parinzee@protonmail.com>
2021-09-30 17:48:30 +0700

    Bumped Ver: Sentry Integration

--- frontend/components/Pages/Credits/Credits.js
+++ frontend/components/Pages/Credits/Credits.js
@@ -57,7 +57,7 @@ export default function Credits() {
 			</InfoTextContainer>
 			<InfoTextContainer>
 				<FrontText>Version</FrontText>
-				<BackText>1.2.7</BackText>
+				<BackText>1.2.8</BackText>
 			</InfoTextContainer>
 			<InfoTextContainer>
 				<FrontText>Created</FrontText>
@@ -65,7 +65,7 @@ export default function Credits() {
 			</InfoTextContainer>
 			<InfoTextContainer>
 				<FrontText>Last Updated</FrontText>
-				<BackText>September 27 2021</BackText>
+				<BackText>Oct 1 2021</BackText>
 			</InfoTextContainer>
 			<InfoTextContainer>
 				<FrontText>Special Thanks</FrontText>
